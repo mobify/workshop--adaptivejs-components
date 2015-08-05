e053133b0854240d10b8fd10bc3c12dc2582f926
Jeff Kamo <kamojeffrey@gmail.com>
2015-08-05 11:14:41 -0700

    Add a blank adaptive project

---
+++ my-project/tasks/config/sass.js
@@ -0,0 +1,54 @@
+module.exports = function(grunt) {
+    return {
+        prod: {
+            options: {
+                bundleExec: true,
+                style: 'compressed',
+                sourcemap: 'none',
+                defaultEncoding: 'utf-8',
+                loadPath: [
+                    'app/',
+                    'app/bower_components/'
+                ],
+                require: [
+                    'compass/import-once/activate'
+                ]
+            },
+            files: [{
+                expand: true,
+                cwd: 'app/',
+                src: [
+                    '**/*.scss',
+                    '!bower_components/**/*.scss'
+                ],
+                dest: 'build/css',
+                ext: '.min.css',
+            }],
+        },
+        dev: {
+            options: {
+                bundleExec: true,
+                style: 'expanded',
+                sourcemap: 'auto',
+                defaultEncoding: 'utf-8',
+                loadPath: [
+                    'app/',
+                    'app/bower_components/'
+                ],
+                require: [
+                    'compass/import-once/activate'
+                ]
+            },
+            files: [{
+                expand: true,
+                cwd: 'app/',
+                src: [
+                    '**/*.scss',
+                    '!bower_components/**/*.scss'
+                ],
+                dest: 'build/css',
+                ext: '.css',
+            }],
+        }
+    };
+};
